eb4b69a5995e285cba2cdbeee8923f9aced58694
Jason <cameljeet@gmail.com>
2015-05-02 15:28:54 -0700

    Rendering a single campaign on click like the show view

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,12 +7,37 @@ $(document).ready(function(){
       alert("Sorry! Couldn't Load Campaign");
     },
     success: function(data){
-      var template = $("#campaign-listing-template").html();
+      var allCampaignsTemplate = $("#campaign-listing-template").html();
       for(var i = 0; i < data.length; i++){
         var campaign = data[i];
-        var html = Mustache.render(template, campaign);
+        var html = Mustache.render(allCampaignsTemplate, campaign);
         $(".campaigns").append(html);
       }
     },
-  })
+  });
+  $(".campaigns").on("click", ".campaign h2 a", function(){
+    var id = $(this).data("id");
+    var show_url = "http://localhost:3000/campaigns/" + id + ".json"; 
+    $.ajax({
+      url: show_url,
+      method: "get",
+      dataType: "json",
+      error: function(){ alert("Error")},
+      success: function(data){
+        console.log(data); // this can be removed
+        var singleCampaignsTemplate = $("#single-campaign-template").html();
+        var html = Mustache.render(singleCampaignsTemplate, data);
+        $(".campaigns").fadeOut("slow", function(){
+          $(".single-campaign").html(''); 
+          $(".single-campaign").append(html);
+          var reward_levels = data.reward_levels;
+          var rewardLevelsTemplate = $("#reward-levels-template").html();
+          for(var i = 0; i < reward_levels.length; i++){
+            var reward_html = Mustache.render(rewardLevelsTemplate, reward_levels[i]);
+            $(".reward-levels").append(reward_html);
+          }
+        });
+      }
+    });
+  });
 });
